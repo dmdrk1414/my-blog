040af3718cfb00361c37b5515dafb998bd63c785
root <root@DESKTOP-N52NBS3.localdomain>
2022-02-13 21:26:18 +0900

    express 로 리액트 홈페이지 띄우기는 가능하다

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,13 @@ const path = require('path');
 const app = express();
 
 const http = require('http').createServer(app);
+
+app.use(express.static(path.join(__dirname, '../../build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../build/index.html'));
+});
+
 http.listen(8080, function () {
     console.log('listening on 8080');
 });
